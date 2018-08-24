9f2ef8ba896166ef2c1ec07f6d2cb60dde3f19d8
Jossan <Jossan@DESKTOP-T47CKMT>
2018-08-24 09:35:06 +0200

    Josefine wrong CVC

--- MatchiJunitSuite/src/selenium/MatchiSelenium.java
+++ MatchiJunitSuite/src/selenium/MatchiSelenium.java
@@ -27,16 +27,12 @@ public class MatchiSelenium {
 	}
 
 	public String checkLogin() {
-		
-		//returnerar texten vid bilden av användaren (I vårt fall "mjuk")
 		WebElement e = webDriver.findElement(By.linkText("Mjuk"));
 		return e.getText();
 	}
 
 	public void chooseSport(String sport) {
 		WebElement e;
-		
-		//Denna metod funkar ej än!
 		e = webDriver.findElement(By.cssSelector(
 				"#hero > div > div > div.row > div > form > div > div.form-group.col-sm-3.col-xs-12.no-margin-padding > div > button > span.filter-option.pull-left"));
 		e.click();
@@ -74,9 +70,7 @@ public class MatchiSelenium {
 	}
 
 	public void login(String username, String password) {
-		
-		
-		//Click "Logga in" at top menu and then fill in name and password
+
 		webDriver.findElement(By.cssSelector("#navbar-collapse > ul.nav.navbar-nav.navbar-right > li:nth-child(2) > a"))
 				.click();
 		WebElement e = webDriver.findElement(By.cssSelector("#username"));
@@ -85,46 +79,71 @@ public class MatchiSelenium {
 		e = webDriver.findElement(By.cssSelector("#password"));
 		e.click();
 		e.sendKeys(password);
-		
-		//Click "Logga in"
 		webDriver.findElement(By.cssSelector("#loginForm > button")).click();
 	}
 
 	public void bookCourt(String search) {
-		
-		
-		//Click "anläggningar"
-		webDriver.findElement(By.xpath("//a[contains(text(),'Anläggningar')]")).click();
-		//
-		
-		//Search for court
+
+		webDriver.findElement(By.cssSelector("#navbar-collapse > ul.nav.navbar-nav.navbar-left > li:nth-child(2) > a"))
+				.click();
 		webDriver.findElement(By.id("q")).click();
 		webDriver.findElement(By.id("q")).clear();
 		webDriver.findElement(By.id("q")).sendKeys(search);
-		webDriver.findElement(By.xpath("//input[@id='submit']")).click();
-		//
-		
-		//Click the searched court
+		webDriver.findElement(By.cssSelector("#submit")).click();
 		webDriver.findElement(By.linkText(search)).click();
-		//
-		
-		//Click at "22:00" Bana 2
-		//(Only works if the browser shows the white squares
-		//and not the squares with time in them 
-		//(These switch based on browser width)
-		webDriver.findElement(By.xpath(
-				"//tbody//tr[3]//td[2]//table[1]//tbody[1]//tr[1]//td[16]"))
+		webDriver.findElement(By.cssSelector(
+				"#schedule > div > div > div:nth-child(2) > table > tbody > tr:nth-child(3) > td:nth-child(2) > table > tbody > tr > td:nth-child(15)"))
 				.click();
 		delay(2000);
-		
+		webDriver.findElement(By.cssSelector("#confirmForm > div.modal-body > div:nth-child(5) > div.col-sm-8.col-xs-8 > div.form-group.no-bottom-margin > div > label"))
+				.click();
+		webDriver.findElement(By.cssSelector("#btnSubmit")).click();
+		delay(500);
+
 
 	}
 	
 	public void paymentCard() {
 		
+		delay(5000);
+		
+		WebElement e;
+		e = webDriver.findElement(By.xpath("//input[@placeholder='Kortnummer']"));
+		e.click();
+		e.sendKeys("2223000048410010");
+		e = webDriver.findElement(By.cssSelector("#adyen-encrypted-form > div.modal-body.relative > div > div > div:nth-child(2) > div:nth-child(2) > input"));
+		e.click();
+		e.sendKeys("Mjuk Varutestare");
+		//e = webDriver.findElement(By.cssSelector("#adyen-encrypted-form > div.modal-body.relative > div > div > div:nth-child(2) > div:nth-child(3) > select"));
+		//e.click();
+		webDriver.findElement(By.xpath("//option[@value='10']")).click();
+		webDriver.findElement(By.xpath("//option[@value='2020']")).click();
+		webDriver.findElement(By.xpath("//option[@value='2020']")).click();
+		e = webDriver.findElement(By.xpath("//input[@placeholder='cvc / cid']"));
+		e.click();
+		e.sendKeys("737");
+		webDriver.findElement(By.xpath("//input[@value='Slutför betalning']")).click();
+		}
+		
+	public void paymentSwish(String mobileNr) {
+		WebElement e = webDriver.findElement(By.cssSelector("#adyen-encrypted-form > div.modal-body.relative > div > div > div:nth-child(2) > div.form-group.col-sm-12.has-feedback.has-feedback-right.has-error > div > input"));
+		//e.click();
+		e.sendKeys("2223000048410010");
+		e = webDriver.findElement(By.cssSelector("#adyen-encrypted-form > div.modal-body.relative > div > div > div:nth-child(2) > div:nth-child(2) > input"));
+		e.click();
+		e.sendKeys("Mjuk Varutestare");
+		e = webDriver.findElement(By.cssSelector("#adyen-encrypted-form > div.modal-body.relative > div > div > div:nth-child(2) > div:nth-child(3) > select"));
+		e.click();
+		webDriver.findElement(By.linkText("10")).click();
+		
+		
+		}
+	
+	public void paymentCardWrongCVC() {
+		
 		delay(1500);
 		
-//
+		//
 		
 		//Click "Nytt kort" if the use an existing card exist
 		//(If use an existing card doesn't exist it selects "Nytt kort" by default
@@ -156,39 +175,22 @@ public class MatchiSelenium {
 		//Enter year
 		webDriver.findElement(By.xpath("//option[@value='2020']")).click();
 		
-		//Enter cvc
+		//Enter wrong CVC
 		e = webDriver.findElement(By.xpath("//input[@placeholder='cvc / cid']"));
 		e.click();
-		e.sendKeys("737");
-		
-		//Click "Slutför betalning"
+		e.sendKeys("738");
 		webDriver.findElement(By.xpath("//input[@value='Slutför betalning']")).click();
-		delay(500);
-		webDriver.findElement(By.xpath("//a[@class='btn btn-success']")).click();
-		//
 		
 		delay(400);
-		
+		}
+
+		public boolean checkWrongCVCNumber(String checkCVC) {
+			WebElement element = webDriver.findElement(By.xpath("//*[@id=\"userBookingModal\"]/div[1]/div/div[2]/h6"));
+			String text = element.getText();
+			System.out.println(text);
+			return text.contains(checkCVC);
 		}
 		
-	public void paymentSwish(String mobileNr) {
-		
-		delay(1500);
-		// Click "Swish" 
-		webDriver.findElement(By.xpath("(.//*[normalize-space(text()) and normalize-space(.)='Nytt konto-/kreditkort'])[1]/following::label[1]")).click();
-		webDriver.findElement(By.id("btnSubmit")).click();
-		// Klicka i mobilnummer rutan
-		webDriver.findElement(By.id("swish.telephoneNumber")).click();
-		webDriver.findElement(By.id("swish.telephoneNumber")).clear();
-		//Skriv in ett test mobilnr
-		webDriver.findElement(By.id("swish.telephoneNumber")).sendKeys(mobileNr);
-		webDriver.findElement(By.id("mainSubmit")).click();
-		delay(2000);
-		// Här failar Swish betalningen
-		// TO-DO höra på fredag varför det failar och skriva klart swish betalningen
-		
-		
-		}
 	public void unbook() {
 		
 		
@@ -207,27 +209,13 @@ public class MatchiSelenium {
 		webDriver.findElement(By.xpath("//button[@id='cancelCloseBtn']")).click();
 		
 	}
-		
-		
 
-	public boolean checkIfBooked() {
-		
-		
-		//Se ifall det finns en ikon på schemaknappen längst upp på sidan
-		try {
-		webDriver.findElement(By.xpath("//span[@class='badge']"));
-		} catch (Exception e) {
-			return false;
-		}
-		return true;
-		
-		
+	public String checkIfBooked() {
+		return webDriver.findElement(By.cssSelector("#userBookingModal > div.modal-dialog > div > div.modal-body > h1"))
+				.getText();
 
 	}
-	/**
-	 * Lägger till en paus i testet
-	 * @param milliseconds
-	 */
+
 	public void delay(int milliseconds) {
 		try {
 			Thread.sleep(milliseconds);
