796c5fcec856c50bdfbfc621d4f1c4d11c664a08
UlfMoberg <ulf.moberg@gmail.com>
2018-09-05 10:20:44 +0200

    tog bort select import

--- MatchiJunitSuite/src/selenium/MatchiSelenium.java
+++ MatchiJunitSuite/src/selenium/MatchiSelenium.java
@@ -9,7 +9,6 @@ import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.ui.Select;
 
 public class MatchiSelenium {
 
